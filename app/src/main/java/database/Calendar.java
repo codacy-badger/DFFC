976829263d6b9bfb7fb7845f1895801f25e46eb3
Florian Christof <florian@gaia-net.de>
2017-11-28 17:00:33 +0100

    WMIT-25 Database  - DB Helper  - CRUDs for calendar

---
+++ app/src/main/java/database/Calendar.java
@@ -0,0 +1,11 @@
+package database;
+
+/**
+ * Created by User on 28.11.2017.
+ */
+
+public class Calendar {
+    public String name;
+    public String description;
+    public boolean isActive;
+}
